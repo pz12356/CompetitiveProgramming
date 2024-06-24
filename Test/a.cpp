371a0e1d57041d62bc8647253aebaf513d102b90
pppzz <387621994@qq.com>
2024-06-24 21:40:47 +0800

    First Commit

---
+++ Test/a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int n;
+  std::cin >> n;
+  for (int i = 0; i < n; i++) {
+    std::cout << "Hello\n";
+  }
+
+  return 0;
+}
