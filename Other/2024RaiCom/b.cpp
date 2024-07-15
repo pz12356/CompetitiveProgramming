e3d9f0974ef4704fb8b15d42ff82284c46ecd834
pppzz <387621994@qq.com>
2024-07-15 17:29:28 +0800

    Add 2024RaiCom

--- Other/2024RaiCom/b.cpp
+++ Other/2024RaiCom/b.cpp
@@ -1,45 +1,45 @@
 #include <bits/stdc++.h>
 
 int RankScore(int x) {
-	if (x == 1) 
-		return 12;
-	if (x == 2) 
-		return 9;
-	if (x == 3)
-		return 7;
-	if (x == 4)
-		return 5;
-	if (x == 5)
-		return 4;
-	if (x >= 6 && x <= 7) 
-		return 3;
-	if (x >= 8 && x <= 10) 
-		return 2;
-	if (x >= 11 && x <= 15) 
-		return 1;
-	return 0;
+  if (x == 1)
+    return 12;
+  if (x == 2)
+    return 9;
+  if (x == 3)
+    return 7;
+  if (x == 4)
+    return 5;
+  if (x == 5)
+    return 4;
+  if (x >= 6 && x <= 7)
+    return 3;
+  if (x >= 8 && x <= 10)
+    return 2;
+  if (x >= 11 && x <= 15)
+    return 1;
+  return 0;
 }
 
 int main() {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(nullptr);
-	
-	int n;
-	std::cin >> n;
-	
-	std::vector<int> score(21);
-	
-	for (int i = 0; i < n; i++) {
-		for (int j = 1; j <= 20; j++) {
-			int p, k;
-			std::cin >> p >> k;
-			
-			score[j] += k + RankScore(p);
-		}
-	}
-	
-	for (int i = 1; i <= 20; i++) {
-		std::cout << i << " " << score[i] << "\n";
-	}
-	return 0;
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int n;
+  std::cin >> n;
+
+  std::vector<int> score(21);
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 1; j <= 20; j++) {
+      int p, k;
+      std::cin >> p >> k;
+
+      score[j] += k + RankScore(p);
+    }
+  }
+
+  for (int i = 1; i <= 20; i++) {
+    std::cout << i << " " << score[i] << "\n";
+  }
+  return 0;
 }
--- Other/2024RaiCom/c.cpp
+++ Other/2024RaiCom/c.cpp
@@ -1,61 +1,61 @@
 #include <bits/stdc++.h>
 
 int main() {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(nullptr);
-	
-	int n, m;
-	std::cin >> n >> m;
-	
-	std::vector<std::string> s(n);
-	for (int i = 0; i < n; i++) {
-		std::cin >> s[i];
-	}
-	
-	std::vector vis(n, std::vector<bool>(m));
-	
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (s[i][j] == 'm') {
-				for (int a = -1; a <= 1; a++) {
-					for (int b = -1; b <= 1; b++) {
-						if (i + a >= 0 && i + a < n && j + b >= 0 && j + b < m) {
-							vis[i + a][j + b] = true;
-						}
-					}
-				}
-			}
-		} 
-	}
-	
-	std::set<std::pair<int, int>> st;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (s[i][j] == '.') {
-				for (int a = -1; a <= 1; a++) {
-					for (int b = -1; b <= 1; b++) {
-						if (i + a >= 0 && i + a < n && j + b >= 0 && j + b < m) {
-							if(vis[i + a][j + b] == false && s[i + a][j + b] == 'w') {
-								st.insert({i, j});
-							}
-							if (s[i + a][j + b] == 'c') {
-								st.erase({i, j});
-								goto flag;
-							}
-						}
-					}
-				}
-			}
-			flag:
-				continue;
-		} 
-	}
-	if (st.empty()) {
-		std::cout << "Too cold!\n";
-	} else {
-		for (auto [x, y] : st) {
-			std::cout << x + 1 << ' ' << y + 1 << '\n';
-		}
-	}
-	return 0;
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int n, m;
+  std::cin >> n >> m;
+
+  std::vector<std::string> s(n);
+  for (int i = 0; i < n; i++) {
+    std::cin >> s[i];
+  }
+
+  std::vector vis(n, std::vector<bool>(m));
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (s[i][j] == 'm') {
+        for (int a = -1; a <= 1; a++) {
+          for (int b = -1; b <= 1; b++) {
+            if (i + a >= 0 && i + a < n && j + b >= 0 && j + b < m) {
+              vis[i + a][j + b] = true;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  std::set<std::pair<int, int>> st;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (s[i][j] == '.') {
+        for (int a = -1; a <= 1; a++) {
+          for (int b = -1; b <= 1; b++) {
+            if (i + a >= 0 && i + a < n && j + b >= 0 && j + b < m) {
+              if (vis[i + a][j + b] == false && s[i + a][j + b] == 'w') {
+                st.insert({i, j});
+              }
+              if (s[i + a][j + b] == 'c') {
+                st.erase({i, j});
+                goto flag;
+              }
+            }
+          }
+        }
+      }
+    flag:
+      continue;
+    }
+  }
+  if (st.empty()) {
+    std::cout << "Too cold!\n";
+  } else {
+    for (auto [x, y] : st) {
+      std::cout << x + 1 << ' ' << y + 1 << '\n';
+    }
+  }
+  return 0;
 }
