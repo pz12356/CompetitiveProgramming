a444df4f5521562dba7d5a5e1b516a88f61a6c5b
pppzz <387621994@qq.com>
2024-08-14 19:08:30 +0800

    Update SparseTable

--- Templates/SparseTable.cpp
+++ Templates/SparseTable.cpp
@@ -1,22 +1,30 @@
-#include <vector>
 #include <algorithm>
+#include <vector>
 
-template <class T>
-struct SparseTable {
-    int n;
-    std::vector<std::vector<T>> a;
-    SparseTable(const std::vector<T> &init) : n(init.size()) {
-        int lg = std::__lg(n);
-        a.assign(lg + 1, std::vector<T>(n));
-        a[0] = init;
-        for (int i = 1; i <= lg; i++) {
-            for (int j = 0; j <= n - (1 << i); j++) {
-                a[i][j] = std::max(a[i - 1][j],  a[i - 1][(1 << (i - 1)) + j]);
-            }
-        }  	    
+template <class T, class F> struct SparseTable {
+  int n;
+  std::vector<std::vector<T>> a;
+  constexpr static F f = F();
+  SparseTable(const std::vector<T> &init) : n(init.size()) {
+    int lg = std::__lg(n);
+    a.assign(lg + 1, std::vector<T>(n));
+    a[0] = init;
+    for (int i = 1; i <= lg; i++) {
+      for (int j = 0; j <= n - (1 << i); j++) {
+        a[i][j] = f(a[i - 1][j], a[i - 1][(1 << (i - 1)) + j]);
+      }
     }
-    T get(int l, int r) {
-        int lg = std::__lg(r - l + 1);
-        return std::max(a[lg][l], a[lg][r - (1 << lg) + 1]);
-    }
+  }
+  T get(int l, int r) {
+    int lg = std::__lg(r - l + 1);
+    return f(a[lg][l], a[lg][r - (1 << lg) + 1]);
+  }
+};
+
+template <class T> struct Max {
+  T operator()(const T &x, const T &y) const { return std::max(x, y); }
+};
+
+template <class T> struct Min {
+  T operator()(const T &x, const T &y) const { return std::min(x, y); }
 };
