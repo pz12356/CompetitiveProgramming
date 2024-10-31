f6cd876b0923dc409ac3bfdb49dc7c65e86df26f
pppzz <387621994@qq.com>
2024-10-31 17:09:36 +0800

    Modify DSU.cpp 	DSU With RollBack

--- Templates/DSU.cpp
+++ Templates/DSU.cpp
@@ -1,43 +1,42 @@
-#include <vector>
+#include <array>
 #include <numeric>
+#include <vector>
 
 struct DSU {
-    std::vector<int> f, siz;
-    
-    DSU() {}
-    DSU(int n) {
-        init(n);
+  std::vector<int> f, siz;
+  std::vector<std::array<int, 2>> his;
+
+  DSU(int n) : siz(n + 1, 1), f(n + 1) { std::iota(f.begin(), f.end(), 0); }
+
+  int find(int x) {
+    while (x != f[x]) {
+      x = f[x] = f[f[x]];
     }
-    
-    void init(int n) {
-        f.resize(n);
-        std::iota(f.begin(), f.end(), 0);
-        siz.assign(n, 1);
+    return x;
+  }
+
+  bool same(int x, int y) { return find(x) == find(y); }
+
+  bool merge(int x, int y) {
+    x = find(x);
+    y = find(y);
+    if (x == y) {
+      return false;
     }
-    
-    int find(int x) {
-        while (x != f[x]) {
-            x = f[x] = f[f[x]];
-        }
-        return x;
+    his.push_back({x, y});
+    siz[x] += siz[y];
+    f[y] = x;
+    return true;
+  }
+
+  int size(int x) { return siz[find(x)]; }
+  int time() { return his.size(); }
+  void revert(int tm) {
+    while (his.size() > tm) {
+      auto [x, y] = his.back();
+      his.pop_back();
+      f[y] = y;
+      siz[x] -= siz[y];
     }
-    
-    bool same(int x, int y) {
-        return find(x) == find(y);
-    }
-    
-    bool merge(int x, int y) {
-        x = find(x);
-        y = find(y);
-        if (x == y) {
-            return false;
-        }
-        siz[x] += siz[y];
-        f[y] = x;
-        return true;
-    }
-    
-    int size(int x) {
-        return siz[find(x)];
-    }
+  }
 };
